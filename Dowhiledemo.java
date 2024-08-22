c3641256c880da5116ede1df676f7ae7456c5224
Rekha978 <152130211+Rekha978@users.noreply.github.com>
2024-08-22 11:42:56 +0530

    Create Dowhiledemo.java

---
+++ Dowhiledemo.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class Dowhiledemo
+{
+    public static void main(String args[])
+    {
+        int i=0;
+        do{
+            System.out.println("Hello World");
+            i++;
+           } while(i<3);
+        
+    }
+}
